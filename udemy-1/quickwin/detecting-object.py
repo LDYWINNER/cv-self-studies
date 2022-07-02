1bb12b5dfb5e95cd121e4067540638f87c3385f2
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-07-02 18:28:10 +0900

    first exercise - quick win

--- udemy-1/quickwin/detecting-object.py
+++ udemy-1/quickwin/detecting-object.py
@@ -19,33 +19,50 @@ File: detecting-object.py
 # Result:
 # Window with Detected Object, Bounding Box and Label in Real Time
 
+# ~= Detect object by using mask that we have found in previous step
+# Then, we will draw bounding box and label
+# We will detect blue object with chosen color mask
 
 # Importing needed library
 import cv2
 
 
 # Defining lower bounds and upper bounds of founded Mask
+# ~= founded color mask numbers from previous step and by that we define range of minimum and maximum numbers
+# to threshold with.
 min_blue, min_green, min_red = 21, 222, 70
 max_blue, max_green, max_red = 176, 255, 255
 
 # Getting version of OpenCV that is currently used
-# Converting string into the list by dot as separator
-# and getting first number
+# Converting string into the list by dot as separator and getting first number
+# ~=And we get here version of opencv that is currently used (needed later).
+# we get from the string list of elements by using split() and dot as separator.
 v = cv2.__version__.split('.')[0]
 
 # Defining object for reading video from camera
+# ~= We are going to detect object in real time, so we need to read frames from camera by VideoCapture() function.
+# It has argument which is number denoting index of camera.
+# By 0, we usually get access to built-in camera.
+# If you have additional camera try to use 1 instead.
+#
 camera = cv2.VideoCapture(0)
 
 
 # Defining loop for catching frames
+# ~= Now we start while loop and catch frame by frame in real time.
 while True:
     # Capture frame-by-frame from camera
+    # ~= We use function read() that gives us result of catching current frame 'True or False' - '_' and
+    # BGR frame itself in form of numpy array.
     _, frame_BGR = camera.read()
 
     # Converting current frame to HSV
+    # ~= As we did in previous step, we convert current frame into HSV color space
     frame_HSV = cv2.cvtColor(frame_BGR, cv2.COLOR_BGR2HSV)
 
     # Implementing Mask with founded colours from Track Bars to HSV Image
+    # ~= implement thresholding with chosen color mask numbers.
+    # As a result, we get binary image with black background and white object.
     mask = cv2.inRange(frame_HSV,
                        (min_blue, min_green, min_red),
                        (max_blue, max_green, max_red))
@@ -69,6 +86,15 @@ while True:
     # We are interested only in Contours
 
     # Checking if OpenCV version 3 is used
+    # ~= With binary image, we can now implement function findContours() that gives us
+    # list of found contours each in form of numpy array with boundary points of object.
+    # We need to be careful here because different versions of OpenCV return different number of parameters
+    # when using function findContours().
+    # That's why we need to check firstly which version of OpenCV is currently used.
+    # If it is third version, then function returns 3 parameters:
+    # 1. modified image 2. list of found contours each in form of numpy array with boundary points of Object
+    # 3. hierarchy
+    # We are only interested in contours.
     if v == '3':
         _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
@@ -78,22 +104,39 @@ while True:
     # Each individual Contour is a Numpy array of(x, y) coordinates
     # of the boundary points of the Object
     # We are interested only in Contours
+    # ~= If it is fourth version, then function returns only 1. found contours each in form of numpy array with
+    # boundary points of object and 2. hierarchy.
+    # Again, we are interested only in contours.
 
     # Checking if OpenCV version 4 is used
     else:
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     # Finding the biggest Contour by sorting from biggest to smallest
+    # ~= After we get all found contours, we sort them according to the size of contour area
+    # and from biggest to smallest.
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
     # Extracting Coordinates of the biggest Contour if any was found
+    # ~= 'if contours': Now we check if in the current frame caught from camera any contour was found.
+    # If yes, we extract coordinates of approximate rectangle around object by function boundingRect().
     if contours:
-        # Getting rectangle coordinates and spatial size from biggest Contour
+        # Getting rectangle coordinates and spatial size from the biggest Contour
         # Function cv2.boundingRect() is used to get an approximate rectangle
         # around the region of interest in the binary image after Contour was found
+        # ~= We pass to function boundingRect() the first contour from the list which is the biggest one.
+        # And we get left upper point: x_min, y_min and box_width & box_height.
+        # We get everything we need to draw bounding box around found object.
         (x_min, y_min, box_width, box_height) = cv2.boundingRect(contours[0])
 
         # Drawing Bounding Box on the current BGR frame
+        # ~= We use function rectangle() and pass current BGR frame, because we want to draw rectangle on
+        # the initial frame that is without any processing.
+        # We pass left upper point x_min, y_min plus some shifting in order to make sure that
+        # whole object is inside the box.
+        # Then, we pass right bottom point: x_min + box_width, y_min + box_height.
+        # Also, we pass color of the box line (0, 255, 0) which is green.
+        # And we pass thickness of the line which is 3.
         cv2.rectangle(frame_BGR, (x_min - 15, y_min - 15),
                       (x_min + box_width + 15, y_min + box_height + 15),
                       (0, 255, 0), 3)
@@ -102,12 +145,23 @@ while True:
         label = 'Detected Object'
 
         # Putting text with Label on the current BGR frame
+        # ~= We now need to put label.
+        # We use function putText() where we pass the same initial BGR frame, label's text and coordinates
+        # as starting point to type the text.
+        # Also, we specify font style, font size, color which is the same with color of box line, and thickness
+        # which is 2.
+        # When current frame is ready to be shown, we go outside if condition and show current frame.
         cv2.putText(frame_BGR, label, (x_min - 5, y_min - 25),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
 
     # Showing current BGR frame with Detected Object
     # Giving name to the window with Detected Object
     # And specifying that window is resizable
+    # ~= We do it outside the if condition in order anyway to show current frame
+    # with or without bounding box around object.
+    # It can be happened that we don't show any blue object, and it can't be found.
+    # But, we still can see what is happening now.
+
     cv2.namedWindow('Detected Object', cv2.WINDOW_NORMAL)
     cv2.imshow('Detected Object', frame_BGR)
 
--- udemy-1/quickwin/finding-mask.py
+++ udemy-1/quickwin/finding-mask.py
@@ -1,5 +1,4 @@
 
-
 """
 Course:  Training YOLO v3 for Objects Detection with Custom Data
 
@@ -19,11 +18,11 @@ File: finding-mask.py
 # max_blue, max_green, max_red = 176, 255, 255
 
 
-# Importing needed library
+# Importing needed library - opencv library
 import cv2
 
 
-# Preparing Track Bars
+# Preparing Track Bars (that will help us to choose numbers for color mask)
 # Defining empty function
 def do_nothing(x):
     pass
@@ -31,9 +30,17 @@ def do_nothing(x):
 
 # Giving name to the window with Track Bars
 # And specifying that window is resizable
+# --> Window itself is defined here
 cv2.namedWindow('Track Bars', cv2.WINDOW_NORMAL)
 
 # Defining Track Bars for convenient process of choosing colours
+# first argument: specify name of track bar itself
+# second argument: specify name of window in which it is going to be displayed
+# Then, we set range of numbers from zero to maximum 255
+# Also, we have to pass an argument function that will do something with chosen number
+# That's why we define here an empty functon (do_nothing) because we are interested only in numbers
+# without any processing
+
 # For minimum range
 cv2.createTrackbar('min_blue', 'Track Bars', 0, 255, do_nothing)
 cv2.createTrackbar('min_green', 'Track Bars', 0, 255, do_nothing)
@@ -44,24 +51,29 @@ cv2.createTrackbar('max_blue', 'Track Bars', 0, 255, do_nothing)
 cv2.createTrackbar('max_green', 'Track Bars', 0, 255, do_nothing)
 cv2.createTrackbar('max_red', 'Track Bars', 0, 255, do_nothing)
 
-# while True:
-#     if cv2.waitKey(0):
-#         break
-
-# Reading image with OpenCV library
+# Reading images with OpenCV library
 # In this way image is opened already as numpy array
 # WARNING! OpenCV by default reads images in BGR format
+# ~= read input image by opencv function that give us BGR image in form of numpy array
 image_BGR = cv2.imread('objects-to-detect.jpg')
 # Resizing image in order to use smaller windows
+# ~= we scale down it with resize() function in order to use more convenient smaller window
 image_BGR = cv2.resize(image_BGR, (600, 426))
 
 # Showing Original Image
 # Giving name to the window with Original Image
 # And specifying that window is resizable
 cv2.namedWindow('Original Image', cv2.WINDOW_NORMAL)
+# we show original image by function imshow() in which we pass name of the window and image itself as arguments
 cv2.imshow('Original Image', image_BGR)
+# When we create opencv window we can pass argument WINDOW_NORMAL that will give us an opportunity to change
+# size of the window when it's opened
 
 # Converting Original Image to HSV
+# ~= Now we convert input image from BGR to HSV color space
+# HSV stands for [Hue Saturation Value]
+# --> With this color space, it is very easy to detect object based on its color
+# By changing value property, we keep needed colors and omit unneeded colors
 image_HSV = cv2.cvtColor(image_BGR, cv2.COLOR_BGR2HSV)
 
 # Showing HSV Image
@@ -70,8 +82,13 @@ image_HSV = cv2.cvtColor(image_BGR, cv2.COLOR_BGR2HSV)
 cv2.namedWindow('HSV Image', cv2.WINDOW_NORMAL)
 cv2.imshow('HSV Image', image_HSV)
 
+# while True:
+#     if cv2.waitKey(0):
+#         break
 
 # Defining loop for choosing right Colours for the Mask
+# ~= Now we start while loop where we define variables to get 6 numbers for color mask according to
+# current position of the appropriate track bar
 while True:
     # Defining variables for saving values of the Track Bars
     # For minimum range
@@ -86,6 +103,8 @@ while True:
 
     # Implementing Mask with chosen colours from Track Bars to HSV Image
     # Defining lower bounds and upper bounds for thresholding
+    # ~= When we have all 6 numbers, we implement thresholding operation by opencv function inRange()
+    # where we pass our HSV image and range of pixel values, from minimum to maximum
     mask = cv2.inRange(image_HSV,
                        (min_blue, min_green, min_red),
                        (max_blue, max_green, max_red))
@@ -93,6 +112,8 @@ while True:
     # Showing Binary Image with implemented Mask
     # Giving name to the window with Mask
     # And specifying that window is resizable
+    # ~= Then, we show resulted binary image in window and repeat this operation in loop
+    # while we find right 6 numbers for color mask
     cv2.namedWindow('Binary Image with Mask', cv2.WINDOW_NORMAL)
     cv2.imshow('Binary Image with Mask', mask)
 
